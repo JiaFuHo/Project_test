7968eef9ebdfee2141d38547a0c060b10314e771
JiaFuHo <james890124@gmail.com>
2025-04-08 20:41:56 +0800

    v2

--- test1/allpage.js
+++ test1/allpage.js
@@ -2,3 +2,15 @@
 $("header").load("./header.html");
 // footer
 $("footer").load("./footer.html");
+
+$.ajax({
+    url: "./FAQ.json",
+    dataType: "json",
+
+    success: (data) => {
+        data.forEach((value) => {
+            $("#accordion_btn_" + value.I).html(value.Q);
+            $("#accordion_flush_" + value.I).html(value.A);
+        });
+    }
+})
