172150715fac86f2b62c550b57703a2139066dcf
JiaFuHo <james890124@gmail.com>
2025-04-09 13:32:44 +0800

    v2.2.0

--- test1/allpage.js
+++ test1/allpage.js
@@ -8,9 +8,22 @@ $.ajax({
     dataType: "json",
 
     success: (data) => {
+        let flush_html = ``;
+
         data.forEach((value) => {
-            $("#accordion_btn_" + value.I).html(value.Q);
-            $("#accordion_flush_" + value.I).html(value.A);
+            flush_html += `
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button id="accordion_btn_${value.I}" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_flush_${value.I}" aria-expanded="false" aria-controls="accordion_flush_${value.I}">
+                    ${value.Q}
+                    </button>
+                </h2>
+                <div id="accordion_flush_${value.I}" class="accordion-collapse collapse" data-bs-parent="#accordion_flush">
+                    ${value.A}
+                </div>
+            </div>`;
+            
+            $("#accordion_flush").html(flush_html);
         });
     }
 })
