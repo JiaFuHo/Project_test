55fc34dc241c2d49ffb4a76caad43588229e8771
JiaFuHo <james890124@gmail.com>
2025-06-16 16:52:58 +0800

    v6.2.0

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -9,11 +9,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -22,8 +23,6 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}").WithStaticAssets();
 
 app.Run();
--- test2/test2/wwwroot/js/site.js
+++ test2/test2/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿$("#btn_search")
+    .on("click", () => { $("#box_search").css("display", "inline") })
+    .on("keypress", (e) => {
+        if (e.key === "Enter") {
+            let x = $("#box_search").val();
+
+            $("#box_submit").val(x).trigger("click");
+            $("#box_search").css("display", "none").val(null);
+
+            if ($(".navbar-toggler").is(":visible")) { $(".navbar-toggler").trigger("click") }
+        }
+    })
+
+$(".link").on("click", () => { if ($(".navbar-toggler").is(":visible")) { $(".navbar-toggler").trigger("click") } })
