3988bdf00ca3887bcb2747e2c9acc3648fa581cf
JiaFuHo <james890124@gmail.com>
2025-06-17 21:43:40 +0800

    v6.3.0

--- test2/test2/wwwroot/js/site.js
+++ test2/test2/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿chkSize = () => {
+﻿//header
+$(".link").on("click", () => { if ($(".navbar-toggler").is(":visible")) { $(".navbar-toggler").trigger("click") } })
+
+//modal
+$("#btn_search2").on("click", () => { $("#box_modal2").removeClass("d-none"); $("#box_modal1").addClass("d-none") })
+$("#btn_search4").on("click", () => { $("#box_modal1").removeClass("d-none"); $("#box_modal2").addClass("d-none") })
+
+//footer
+chkSize = () => {
     let x = $(document).width();
 
     if (x <= 768) { $(".box_info").addClass("d-none"); }
@@ -8,18 +16,3 @@
 $(() => { chkSize() })
 
 $(window).on("resize", () => { chkSize() })
-
-$("#btn_search")
-    .on("click", () => { $("#box_search").css("display", "inline") })
-    .on("keypress", (e) => {
-        if (e.key === "Enter") {
-            let x = $("#box_search").val();
-
-            $("#box_submit").val(x).trigger("click");
-            $("#box_search").css("display", "none").val(null);
-
-            if ($(".navbar-toggler").is(":visible")) { $(".navbar-toggler").trigger("click") }
-        }
-    })
-
-$(".link").on("click", () => { if ($(".navbar-toggler").is(":visible")) { $(".navbar-toggler").trigger("click") } })
